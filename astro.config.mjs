356a89d83563d9d0e72ee9059825a6ba8ff84e30
Danny White <3104761+dnywh@users.noreply.github.com>
2024-08-21 22:17:45 +0200

    Basic image examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    image: {
+        domains: ["inaturalist-open-data.s3.amazonaws.com", "flickr.com"],
+    }
+});
