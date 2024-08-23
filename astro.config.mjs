2a0d7c4e0206569031dff148854523db6c712b0c
Danny White <3104761+dnywh@users.noreply.github.com>
2024-08-23 09:35:17 +0200

    Using SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
     image: {
-        domains: ["inaturalist-open-data.s3.amazonaws.com", "flickr.com"],
-    }
+        domains: ["inaturalist-open-data.s3.amazonaws.com", "flickr.com"]
+    },
+    output: "server",
+    adapter: netlify()
 });
